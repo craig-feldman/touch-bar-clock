3403bfe1f798fde99ac7c360c9a044d8d10edfd7
Craig Feldman <craig@overhq.com>
2019-08-11 22:29:29 +0200

    Initial Commit

---
+++ Take a break/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Take a break
+//
+//  Created by Craig Feldman on 2019/08/11.
+//  Copyright © 2019 Craig Feldman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Take a break/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Take a break
+//
+//  Created by Craig Feldman on 2019/08/11.
+//  Copyright © 2019 Craig Feldman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
